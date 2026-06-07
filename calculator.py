8220288bbc06a794dcb161f7c545f973994039d7
vikram vishwkarma <vikramvishwkarma26@gmail.com>
2026-06-07 13:57:07 +0530

    improved calculator and completed day 2 python practice

--- calculator.py
+++ calculator.py
@@ -1,4 +1,8 @@
 x = int(input("what is x"))
 y = int(input(" what is y "))
 
-print(x+y)  
+
+print(f"Additon = {x + y}")  
+print (f"Substraction = {x - y}")
+print (f"Multiplication = {x * y}")
+print (f"Division = { x / y}")
